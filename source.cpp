4c99cdab3ff09069ef3cf04ae720e361dc697ba9
Anais Biscaras <72392239+Loulaty@users.noreply.github.com>
2020-11-16 16:49:13 +0100

    commit

--- source.cpp
+++ source.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-  float vas = 5.5f;
+int main(){
+  float var[15];
+
+  var[3] = 6;
+  int i = 0
+  while (i != 15){
+       if(1== 3 || i == 11) {
+           i++;
+           continue;
+       }
+       else{
+            var [i] = 1
+       }
+       i++;
+  }
+  for (int i = 0; i < 15; i++){
+       cout << var[i]=i << endl
+}
+
   string s ="";
   cin >> s;
   cout << s <<endl;
-  
 }
 
