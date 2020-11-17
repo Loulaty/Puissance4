a6e3d103ea2e1ca3addfba37b6cfd58f2c8fac0d
Anais Biscaras <72392239+Loulaty@users.noreply.github.com>
2020-11-17 18:33:53 +0100

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    /*int tab [6][7] = {0};*/
+    for(int i = 0; i <6; i++){
+        for (int j=0; j < 7; j++){
+            cout <<"| _ |";
+        }
+        cout << endl;
+    }
+}
